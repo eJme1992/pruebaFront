657fa9a03050850380126f150bd2715d4b0b72e6
ejme1992 <edwin.jme@hotmail.com>
2024-06-09 23:16:11 -0300

    feat: Agregar archivo respond.json y utils.js
    Se agregó el archivo respond.json con datos de respuesta y el archivo utils.js con funciones de utilidad para realizar solicitudes HTTP.

--- js/page.js
+++ js/page.js
@@ -1,56 +1,206 @@
+import { surveyJson } from "./survey.js";
+import {
+  BASE_URL,
+  EMAIL_REGEX,
+  THANK_LOTTIE,
+  fetch,
+  buildOptionsByToken,
+} from "./utils.js";
+
+const SURVEY_RESPOND_JSON = {
+  pages: [],
+};
+var Survey = window?.Survey || {};
+
 let timerInterval;
 let milliseconds = 0;
 let seconds = 0;
 let minutes = 0;
-var isActive = false
+var isActive = false;
 
-function startTimer() {
-    clearInterval(timerInterval);
-    timerInterval = setInterval(updateTimer, 10);
+function startTimer(startAt) {
+  clearInterval(timerInterval);
+
+  const {
+    minutes: min = 0,
+    seconds: sec = 0,
+    milliseconds: ms = 0,
+  } = startAt || {};
+
+  minutes = parseInt(min, 10);
+  seconds = parseInt(sec, 10);
+  milliseconds = parseInt(ms, 10);
+
+  timerInterval = setInterval(updateTimer, 10);
 }
 
 function stopTimer() {
-    clearInterval(timerInterval);
+  clearInterval(timerInterval);
 }
 
 function resetTimer() {
-    clearInterval(timerInterval);
-    milliseconds = 0;
-    seconds = 0;
-    minutes = 0;
-    updateTimer();
+  clearInterval(timerInterval);
+  milliseconds = 0;
+  seconds = 0;
+  minutes = 0;
+  updateTimer();
 }
 
 function updateTimer() {
-    milliseconds += 10;
-    if (milliseconds >= 1000) {
-        milliseconds = 0;
-        seconds++;
-        if (seconds >= 60) {
-            seconds = 0;
-            minutes++;
-        }
+  milliseconds += 10;
+  if (milliseconds >= 1000) {
+    milliseconds = 0;
+    seconds++;
+    if (seconds >= 60) {
+      seconds = 0;
+      minutes++;
     }
+  }
 
-    const formattedMilliseconds = pad(milliseconds, 3);
-    const formattedSeconds = pad(seconds, 2);
-    const formattedMinutes = pad(minutes, 2);
+  const formattedMilliseconds = pad(milliseconds, 3);
+  const formattedSeconds = pad(seconds, 2);
+  const formattedMinutes = pad(minutes, 2);
 
-    document.getElementById("timer").innerText = `${formattedMinutes}:${formattedSeconds}:${formattedMilliseconds}`;
+  document.getElementById(
+    "timer"
+  ).innerText = `${formattedMinutes}:${formattedSeconds}:${formattedMilliseconds}`;
+
+  return {
+    minutes: formattedMinutes,
+    seconds: formattedSeconds,
+    milliseconds: formattedMilliseconds,
+  };
 }
 
 function pad(number, width) {
-    return String(number).padStart(width, '0');
+  return String(number).padStart(width, "0");
 }
 
-$('document').ready(function() {
-    var nextBtn = $('.sd-btn sd-navigation__next-btn')
-    nextBtn && nextBtn.addEventListener('click', function() {
-        if (isActive) {
-            startTimer()
-        } else {
-            stopTimer()
-        }
-       
-    })
-})
+$("document").ready(function () {
+  $(".sd-btn sd-navigation__next-btn");
+  if (isActive) {
+    startTimer();
+  } else {
+    stopTimer();
+  }
+});
+
+$(function () {
+  // First Part Identify the user:
+  const email = $("#email");
+  const btnSubmit = $("#submit-btn");
+
+  btnSubmit.on("click", function () {
+    if (email.val() === "" || !EMAIL_REGEX.test(email.val())) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "The Email is incorrect, please introduce a valid email",
+      });
+      return;
+    }
+
+    fetch(
+      `${BASE_URL}/begin`,
+      {
+        method: "POST",
+        body: {
+          email: email.val(),
+          survey: surveyJson.title,
+        },
+      },
+      function (response) {
+        Survey.token = response.data.token;
+        $("#main-section").hide();
+        SurveyInit();
+      }
+    );
+  });
+});
+
+// Survey Handlers
+
+function SurveyInit() {
+  const surveyEl = document.createElement("div");
+  surveyEl.id = "surveyContainer";
+  $(surveyEl).insertAfter("#main-section");
+
+  const survey = new Survey.Model(surveyJson);
+
+  survey.onProcessHtml.add((survey, options) => {
+    options.html = `
+      <div class="survey-thank">${THANK_LOTTIE}<p>Thank you for your Time! :)</p></div>
+    `;
+  });
+
+  survey.onAfterRenderQuestion.add((survey, question) =>
+    registering(survey, question)
+  );
+
+  survey.onCurrentPageChanging.add((survey, options) => {
+    const previousQuestionTime = updateTimer();
+    stopTimer();
+    const { currentPageNo } = survey;
+    const formattedPage = parseInt(currentPageNo);
+    const { oldCurrentPage } = options || {};
+
+    SURVEY_RESPOND_JSON.pages[formattedPage] = {
+      ...oldCurrentPage.jsonObj,
+      time: previousQuestionTime,
+    };
+
+    resetTimer();
+  });
+
+  survey.onComplete.add((sender, options) => {
+    const { data: values, jsonObj } = sender;
+    const { pages } = jsonObj;
+    const lastQuestion = pages.pop();
+    const getLastQuestionTime = updateTimer();
+    stopTimer();
+    resetTimer();
+
+    //Append last page time
+    SURVEY_RESPOND_JSON.pages.push({
+      ...lastQuestion,
+      time: getLastQuestionTime,
+    });
+
+    SURVEY_RESPOND_JSON.pages = SURVEY_RESPOND_JSON.pages.map(
+      (question, indx) => {
+        const q = question;
+        delete q.elements;
+        return {
+          ...q,
+          value: values[`question${indx + 1}`],
+          name: `question${indx + 1}`,
+        };
+      }
+    );
+    const opts = buildOptionsByToken(Survey.token, "POST", SURVEY_RESPOND_JSON);
+    fetch(
+      `${BASE_URL}/registerAnswer`,
+      opts,
+      function (data) {
+        console.log("Success:", data);
+      },
+      function (err) {
+        console.error("Error:", err);
+      }
+    );
+  });
+
+  //Init Survey
+  $("#surveyContainer").Survey({ model: survey });
+}
+
+//Start Counting the time when the question is showed!
+const registering = (survey, question) => {
+  const { currentPageNo } = survey;
+  const page = parseInt(currentPageNo);
+  const previousTime =
+    typeof page === "number" && SURVEY_RESPOND_JSON?.pages?.[page]?.time
+      ? SURVEY_RESPOND_JSON.pages[page]?.time
+      : undefined;
+  startTimer(previousTime);
+};
---
+++ js/utils.js
@@ -0,0 +1,26 @@
+export const EMAIL_REGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+export const BASE_URL = "http://127.0.0.1:8000";
+export function fetch(url, options, success, error) {
+  const { headers = {}, method, body = {}, dataType = "json" } = options;
+  $.ajax({
+    url: url,
+    headers,
+    type: method,
+    data: body,
+    dataType,
+    success: success,
+    error: error,
+  });
+}
+export const THANK_LOTTIE = `<dotlottie-player src="https://lottie.host/82084d97-08de-453b-98b0-bd144b9bf99f/boOvq2CSf1.json" background="transparent" speed="1" style="width: 400px; height: 400px;" loop autoplay></dotlottie-player>`;
+
+export const buildOptionsByToken = (token, method = "POST", data) => {
+  return {
+    method: method,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(data),
+  };
+};
